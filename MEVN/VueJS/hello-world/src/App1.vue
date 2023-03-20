51e7e8fb08e03ba5e0267209d458853165e1c1fe
pmp2601 <83898154+pmp2601@users.noreply.github.com>
2023-03-20 19:23:59 +0530

    Commit

---
+++ MEVN/VueJS/hello-world/src/App1.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { RouterLink, RouterView} from 'vue-router'
+import HelloWorld1 from './components/HelloWorld1.vue'
+import AboutYou from './components/AboutYou.vue'
+import Practice from './components/Practice.vue'
+</script>
+
+<template>
+    <header>
+        <div class="wrapper">
+            <br>
+            <nav>
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+                <RouterLink to="/test">Test</RouterLink>
+                <RouterLink to="/login">Login</RouterLink>
+                <RouterLink to="/aboutyou">AboutYou</RouterLink>
+                <RouterLink to="/products">Products</RouterLink>
+            </nav>
+            <br>
+        </div>
+    </header>
+    <RouterView/>
+</template>
+    
+    <style scoped>
+    header{
+
+    }
+    </style>
+    
